554ac8a6cf4992932cf99880c4a913a913e58095
Hunter Caron <huntercaron@me.com>
2019-08-06 13:21:47 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import { Data } from "framer"
+//@ts-ignore
+import * as firebase from "firebase/app"
+import "firebase/database"
+
+export function FirebaseData(dataObj, config) {
+    let database
+    const data = Data(dataObj)
+
+    const initFirebase = () => {
+        if (!firebase.apps.length) {
+            firebase.initializeApp(config)
+        }
+        database = firebase.database()
+    }
+
+    initFirebase()
+
+    for (let property in data) {
+        database.ref(property).on("value", snapshot => {
+            data[property] = snapshot.val()
+        })
+    }
+
+    var p = new Proxy(data, {
+        get: (target, name) => {
+            return target[name]
+        },
+        set: (obj, prop, value) => {
+            database.ref(prop).set(value)
+            return true
+        },
+    })
+    return p
+}
