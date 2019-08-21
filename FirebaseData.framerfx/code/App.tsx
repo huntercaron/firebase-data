6c1212ee4148e9d9f8b8ac696fd8e766aca59846
Hunter Caron <huntercaron@me.com>
2019-08-21 12:07:26 +0200

    not allowing changing of firebase before init

--- FirebaseData.framerfx/code/App.tsx
+++ FirebaseData.framerfx/code/App.tsx
@@ -2,31 +2,29 @@ import * as React from "react"
 import { Frame, useCycle, Override } from "framer"
 import { FirebaseData } from "./FirebaseData"
 
-const firebaseConfig = {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "framer-firebase.firebaseapp.com",
-    databaseURL: "https://framer-firebase.firebaseio.com",
-    projectId: "framer-firebase",
+var firebaseConfig = {
+    apiKey: "AIzaSyBctPiEporhwRjzZ1TOz89mZOCzN5ramAQ",
+    authDomain: "framer-firebase-116d1.firebaseapp.com",
+    databaseURL: "https://framer-firebase-116d1.firebaseio.com",
+    projectId: "framer-firebase-116d1",
     storageBucket: "",
-    messagingSenderId: "xxxxxxxxxxxx",
-    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "155502761427",
+    appId: "1:155502761427:web:7b78c2d51bdf7462",
 }
 
 const data = FirebaseData(firebaseConfig, {
     userLoggedIn: false,
     accentColor: "#0000ff",
     messages: [{ userID: 1 }],
+    test4: "hehe",
 })
 
 export const Convo: Override = () => ({
     messages: data.messages,
 })
 
-export function Hover(): Override {
-    return {
-        whileHover: { scale: 0.8 },
-        onTap() {
-            data.test = "fuck"
-        },
-    }
-}
+export const NewMessage: Override = () => ({
+    onTap() {
+        data.messages = [...data.messages, { userID: 1 }]
+    },
+})
--- FirebaseData.framerfx/code/FirebaseData.tsx
+++ FirebaseData.framerfx/code/FirebaseData.tsx
@@ -54,22 +54,33 @@ function FirebaseApp(config) {
 export function FirebaseData(config = {}, initialState, ref = "") {
     const { database, app } = FirebaseApp(config)
     const data = Data(initialState)
+    let initialized = false
 
-    for (let prop in data) {
-        const refPath = ref ? `${ref}/${prop}` : prop
+    const intializingProps = Object.keys(initialState).map(prop => {
+        return new Promise((resolve, reject) => {
+            const refPath = ref ? `${ref}/${prop}` : prop
 
-        database.ref(refPath).once("value", snapshot => {
-            if (snapshot.exists()) {
+            database.ref(refPath).once("value", snapshot => {
+                if (snapshot.exists()) {
+                    data[prop] = snapshot.val()
+                    resolve()
+                } else {
+                    database
+                        .ref(refPath)
+                        .set(initialState[prop])
+                        .then(resolve)
+                }
+            })
+
+            database.ref(refPath).on("value", snapshot => {
                 data[prop] = snapshot.val()
-            } else {
-                database.ref(refPath).set(initialState[prop])
-            }
+            })
         })
+    })
 
-        database.ref(refPath).on("value", snapshot => {
-            data[prop] = snapshot.val()
-        })
-    }
+    Promise.all(intializingProps).then(() => {
+        initialized = true
+    })
 
     var p = new Proxy(data, {
         get: (target, name) => {
@@ -77,8 +88,9 @@ export function FirebaseData(config = {}, initialState, ref = "") {
         },
         set: (obj, prop, value) => {
             const refPath = ref ? `${ref}/${String(prop)}` : prop
-            // @ts-ignore
-            database.ref(refPath).set(value)
+            if (initialized)
+                // @ts-ignore
+                database.ref(refPath).set(value)
             return true
         },
     })
